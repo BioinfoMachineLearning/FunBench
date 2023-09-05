1e2d0b8970639e123dddeb6f8181e8a8e418bee4
pengzhangzhi <pengzhangzhics@gmail.com>
2023-09-04 19:13:46 -0500

    [feat] BSpred compute metrics

---
+++ baselines/BSpred/compute_metrics.py
@@ -0,0 +1,83 @@
+import subprocess
+import os
+from glob import glob
+import pandas as pd
+from tqdm import tqdm
+import torch
+
+
+from scoring import bc_score_names, bc_scoring
+
+def cast_name(s):
+    pdb, _, chain, *_ = s.split('_')
+    chain = chain.split('-')[1][0]
+    pdbid = f"{pdb}_{chain}"
+    return pdbid
+
+
+def load_true_labels(label_h5_path):
+    """
+    return a dataframe with columns ['id','true_binding_sites'], 
+    where true_binding_sites is a list of binding sites for each type molecules.
+    
+    """
+    true_df = pd.read_hdf(label_h5_path, key='df')
+    # true_df = true_df[['id','binding_labels']]
+    true_df['binding_labels'] = true_df['binding_labels'].apply(lambda x: x[:,0].float()) # obtain the protein binding labels
+    # rename binding_sites to true_binding_sites
+    true_df.rename(columns={'binding_labels':'true_binding_sites'}, inplace=True)
+    return true_df
+
+def load_pred_labels(pred_path):
+    # if file is empty or not exist, return None
+    if not os.path.exists(pred_path) or os.path.getsize(pred_path) == 0:
+        print(f"{pred_path} is empty or not exist.")
+        return None
+    df = pd.read_csv(pred_path,sep='\t',header=None)
+    return torch.tensor(df.iloc[:,-1].values)
+
+def compute_metrics(label_h5_path, predictions_folder, output_csv):
+    true_df = load_true_labels(label_h5_path)
+    res_df = pd.DataFrame(columns=['id',*bc_score_names])
+    for p in tqdm(glob(os.path.join(predictions_folder, "*"))):
+        # obtain dir name of p
+        pname = os.path.basename(p)
+        res_csv = os.path.join(p,"prediction.txt")
+        
+        pred_labels = load_pred_labels(res_csv)
+        if pred_labels is None:
+            continue
+        pdbid = pname
+        true_df.isin([pdbid])
+        if pdbid not in true_df['id'].values:
+            continue
+        true_labels = true_df[true_df['id']==pdbid]['true_binding_sites'].iloc[0]
+        
+        if true_labels.shape[0] != pred_labels.shape[0]:
+            print(f"{true_labels.shape[0]} != {pred_labels.shape[0]} for {pdbid}")
+            continue
+        metrics_tns = bc_scoring(true_labels.unsqueeze(-1),pred_labels.unsqueeze(-1))
+
+        tmp_df = pd.DataFrame({
+            'id':pdbid, 
+            **dict(zip(bc_score_names, metrics_tns.tolist())),
+            })
+        res_df = pd.concat([res_df, tmp_df], axis=0)
+        
+    res_df.to_csv(output_csv, index=False)
+    return res_df
+    
+
+
+    
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predictions_folder',dest='predictions_folder',
+                        default='./predictions',
+                        help='Input name')
+    parser.add_argument('--label_h5_path',default='/home/zpw97/github-projs/PDBAnnotator/test_df_protein_binding.h5',
+                        help='the h5 file containing the true labels.')
+    parser.add_argument('--output_csv',default='./BSPred_metrics.csv',)
+    args = parser.parse_args()
+    compute_metrics(args.label_h5_path, args.predictions_folder, args.output_csv)
--- baselines/BSpred/eval.py
+++ baselines/BSpred/eval.py
@@ -3,30 +3,43 @@ import os
 from Bio import SeqIO
 
 
+import ray
+
+# Initialize Ray
+ray.init()
+
+@ray.remote
+def process_record(record, save_dir):
+    try:
+        pdb_id = record.id
+        sequence = str(record.seq)
+        pdb_dir = os.path.join(save_dir, pdb_id)
+        os.makedirs(pdb_dir, exist_ok=True)
+        # write the sequence to pdb_dir
+        res_path = os.path.join(pdb_dir, 'prediction.txt')
+        if os.path.exists(res_path):
+            if os.path.getsize(res_path) != 0:
+                return
+        with open(os.path.join(pdb_dir, 'seq.txt'), 'w') as f:
+            f.write(f">{pdb_id}" + "\n" + sequence)
+        subprocess.run(['./INSTALL_BSpred/bin/runBSpred.pl', pdb_dir])
+
+    except KeyboardInterrupt as e:
+        raise e
+    except:
+        # write to file
+        with open('error_pdb_id.txt', 'a') as f:
+            f.write(pdb_id + '\n')
+
 def run_infer_ls(records, save_dir):
     if not os.path.exists('error_pdb_id.txt'):
         with open('error_pdb_id.txt', 'w') as f:
             f.write('')
-            
-    for record in records:
-        try:
-            pdb_id = record.id
-            sequence = str(record.seq)
-            pdb_dir = os.path.join(save_dir, pdb_id)
-            os.makedirs(pdb_dir, exist_ok=True)
-            # write the sequence to pdb_dir
-            with open(os.path.join(pdb_dir, 'seq.txt'), 'w') as f:
-                f.write(f">{pdb_id}"+"\n"+sequence)
-            
-            subprocess.run(['./INSTALL_BSpred/bin/runBSpred.pl', pdb_dir])
-        except KeyboardInterrupt as e:
-            raise e
-        except:
-            # write to file
-            with open('error_pdb_id.txt', 'a') as f:
-                f.write(pdb_id + '\n')
-                
-                
+
+    futures = [process_record.remote(record, save_dir) for record in records]
+    ray.get(futures)  # Wait for all tasks to complete
+
+
 def run_infer_path(a3m, save_dir):
     # read a3m file using bio
     with open(a3m, 'r') as f:
