026beae623e8d2b1b38c4650c07ba27763bcd20a
pengzhangzhi <pengzhangzhics@gmail.com>
2023-09-04 19:13:30 -0500

    [feat] masif compute metrics

--- baselines/masif/eval.py
+++ baselines/masif/eval.py
@@ -10,6 +10,8 @@ def run_infer_ls(pdb_id_list):
         try:
             subprocess.run(['./data_prepare_one.sh', pdb_id, ])
             subprocess.run(['./predict_site.sh', pdb_id, ])
+        except KeyboardInterrupt as e:
+            raise e
         except:
             # write to file
             with open('error_pdb_id.txt', 'a') as f:
@@ -26,8 +28,8 @@ def run_infer_path(pdb_id_path,):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pdb_id_path', type=str, default='../../construct_testset/data/next_pdb_non_redundant_ids.txt')
-    parser.add_argument('--no_msa', action='store_true')
+
+    parser.add_argument('--pdb_id_path', type=str, default='/home/zpw97/github-projs/PDBAnnotator/test_pdb_protein_binding.txt')
 
     args = parser.parse_args()
     run_infer_path(args.pdb_id_path)
